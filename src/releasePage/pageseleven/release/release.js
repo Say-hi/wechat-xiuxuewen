2ef638f1b0cc01d95929601b42d5dbaae61e5fe0
Say-hi <782108446@qq.com>
2018-12-18 18:43:37 +0800

    小程序端直传oss

--- src/releasePage/pageseleven/release/release.js
+++ src/releasePage/pageseleven/release/release.js
@@ -1,7 +1,83 @@
+/*eslint-disable*/
+
 // 获取全局应用程序实例对象
 const app = getApp()
-let task = {}
 let change = true
+let Key = null
+const COS = require('./cos-js-sdk-v5.min')
+const config = require('./config')
+var cos = new COS({
+  getAuthorization: function (params, callback) {//获取签名 必填参数
+
+    // 方法一（推荐）服务器提供计算签名的接口
+    /*
+     wx.request({
+     url: 'SIGN_SERVER_URL',
+     data: {
+     Method: params.Method,
+     Key: params.Key
+     },
+     dataType: 'text',
+     success: function (result) {
+     callback(result.data);
+     }
+     });
+     */
+
+    // 方法二（适用于前端调试）
+    var authorization = COS.getAuthorization({
+      SecretId: config.SecretId,
+      SecretKey: config.SecretKey,
+      Method: params.Method,
+      Key: params.Key
+    });
+    callback(authorization);
+  }
+});
+
+var requestCallback = function (err, data) {
+  console.log(Key)
+  var url = cos.getObjectUrl({
+    Key
+  });
+  console.log(url)
+  if (err && err.error) {
+    wx.showModal({title: '返回错误', content: '请求失败：' + err.error.Message + '；状态码：' + err.statusCode, showCancel: false});
+  } else if (err) {
+    wx.showModal({title: '请求出错', content: '请求出错：' + err + '；状态码：' + err.statusCode, showCancel: false});
+  } else {
+    wx.showToast({title: '请求成功', icon: 'success', duration: 3000});
+  }
+};
+
+var option = {
+  data: {
+    list: [],
+  },
+};
+
+option.simpleUpload = function () {
+  // 选择文件
+  wx.chooseImage({
+    count: 1, // 默认9
+    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+    success: function (res) {
+      var filePath = res.tempFilePaths[0]
+      var Key = 'image/' + filePath.substr(filePath.lastIndexOf('/') + 1); // 这里指定上传的文件名
+
+      cos.postObject({
+        Bucket: config.Bucket,
+        Region: config.Region,
+        Key: Key,
+        FilePath: filePath,
+        onProgress: function (info) {
+          console.log(JSON.stringify(info));
+        }
+      }, requestCallback);
+    }
+  })
+};
 // 创建页面实例对象
 Page({
   /**
@@ -55,7 +131,7 @@ Page({
         if(index) {}
         (function upLoad (j) {
           let v = res.tempFilePaths[j]
-          task = wx.uploadFile({
+          const task = wx.uploadFile({
             url: app.getUrl().upImage,
             filePath: v,
             name: 'file',
@@ -71,6 +147,7 @@ Page({
           })
           task.onProgressUpdate(res => {
             that.data.upImgArrProgress[index >= 0 ? index : length + j] = res.progress
+            console.log(res.progress)
             that.setData({
               upImgArrProgress: that.data.upImgArrProgress
             })
@@ -102,6 +179,10 @@ Page({
         }
       }
     })
+  },
+
+  testUP () {
+    option.simpleUpload()
   },
   /**
    * 生命周期函数--监听页面加载
