48a627cbe32abb17b834748d2836582609617ac6
Say-hi <782108446@qq.com>
2019-01-16 11:33:18 +0800

    add new ruler

--- src/newPage/pagesthrteen/new/new.js
+++ src/newPage/pagesthrteen/new/new.js
@@ -7,12 +7,34 @@ Page({
    * 页面的初始数据
    */
   data: {
-    title: 'new'
+    lvIndex: 0,
+    tabIndex: 0,
+    titleArr: ['第一阶段 夯实基础学技术', '第二阶段：个人素养应提升', '第三阶段：光环效应须“包装”'],
+    videoUrl: 'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
+    testImg: 'https://teach-1258261086.cos.ap-guangzhou.myqcloud.com/image/admin/background/005.png'
+  },
+  showImg (e) {
+    app.showImg(e.currentTarget.dataset.src, [e.currentTarget.dataset.src])
+  },
+  swiperChange (e) {
+    this.setData({
+      tabIndex: e.detail.current
+    })
+  },
+  lvChoose (e) {
+    this.setData({
+      lvIndex: e.currentTarget.dataset.index
+    })
+  },
+  chooseTab (e) {
+    this.setData({
+      tabIndex: e.currentTarget.dataset.index
+    })
   },
   onShareAppMessage () {
     return {
-      title: '绣学问，真纹绣',
-      path: `/pages/index/index`
+      title: '纹饰美容的成长三步曲',
+      path: `/newPage/pagesthrteen/new/new`
     }
   },
   /**
